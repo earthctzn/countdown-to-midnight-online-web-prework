639dbf49f41a68a841999f0bc55b4e65662f2eb7
Caleb Einarr <earthctzn1@gmail.com>
2019-04-04 21:46:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown
- time = 12
-while time > 0
+ counter = 12
+while counter > 0
   puts "#{time} SECOND(S)!"
-  time -= 1
+  counter -= 1 
   "HAPPY NEW YEAR!"
 end
 end
