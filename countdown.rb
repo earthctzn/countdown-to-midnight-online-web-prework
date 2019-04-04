5caab86728e6f1b68bb05b63984fc61eba4ced01
Caleb Einarr <earthctzn1@gmail.com>
2019-04-04 21:58:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,6 +7,15 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(x)
-  sleep x
+#def countdown_with_sleep(x)
+ # sleep x
+#end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
